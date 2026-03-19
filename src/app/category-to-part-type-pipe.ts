db13e2bc633da9ab5c4d36a8e6299787962798c4
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2026-03-18 21:19:45 -0500

    pure vs. impure pipes

--- src/app/category-to-part-type-pipe.ts
+++ src/app/category-to-part-type-pipe.ts
@@ -4,7 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'categoryToPartType',
 })
 export class CategoryToPartTypePipe implements PipeTransform {
-  transform(category: string): string {
-    return category[0].toUpperCase() + category.slice(1, -1);
+  transform(category: string, uppercase: boolean): string {
+    if (uppercase) {
+      return category[0].toUpperCase() + category.slice(1, -1);
+    } else {
+      return category.slice(0, -1);
+    }
   }
 }
---
+++ src/app/filter-by-category-pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IProduct } from './product.model';
+
+@Pipe({
+  name: 'filterByCategory',
+  // pure: false, //avoid this
+})
+export class FilterByCategoryPipe implements PipeTransform {
+  transform(
+    products: IProduct[],
+    category: string | number | null,
+  ): IProduct[] {
+    if (!category) return products;
+    return products.filter((p) => p.category === category);
+  }
+}
